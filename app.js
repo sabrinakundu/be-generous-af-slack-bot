13207b7e495a21730a93fcaf323cb6774c8feb27
Gautam Jethwani <gjethwani@atlassian.com>
2020-08-06 14:47:08 -0700

    Test

--- app.js
+++ app.js
@@ -8,7 +8,39 @@ app.get('/', (req, res) => {
 
 app.post('/slack', (req, res) => {
     console.log(req.body)
-    res.status(200).send("<table>hello</table>")
+    res.status(200).json({
+        "text": "This is your first interactive message",
+        "attachments": [
+            {
+                "text": "Building buttons is easy right?",
+                "fallback": "Shame... buttons aren't supported in this land",
+                "callback_id": "button_tutorial",
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "actions": [
+                    {
+                        "name": "yes",
+                        "text": "yes",
+                        "type": "button",
+                        "value": "yes"
+                    },
+                    {
+                        "name": "no",
+                        "text": "no",
+                        "type": "button",
+                        "value": "no"
+                    },
+                    {
+                        "name": "maybe",
+                        "text": "maybe",
+                        "type": "button",
+                        "value": "maybe",
+                        "style": "danger"
+                    }
+                ]
+            }
+        ]
+    })
 })
 
 app.listen(port, () => {
