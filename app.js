bcfd12b06815537e103660de9999f7b0fc32b836
Gautam Jethwani <gjethwani@atlassian.com>
2020-08-06 15:47:45 -0700

    Body parser

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
