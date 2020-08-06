458d1e3c06b3e488138beccb37d7895028ba57a3
Gautam Jethwani <gjethwani@atlassian.com>
2020-08-06 14:35:02 -0700

    Add post endpoint

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3888
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    console.log(req.body)
   res.send('Hello World!')
+})
+
+app.post('/slack', (req, res) => {
+    console.log(req.body)
+    res.status(200).send()
 })
 
 app.listen(port, () => {
