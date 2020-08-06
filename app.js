1f7ad368fa82db2c7a452df5db0a2c8d0cf5bbaf
Gautam Jethwani <gjethwani@atlassian.com>
2020-08-06 15:55:00 -0700

    change bp

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
